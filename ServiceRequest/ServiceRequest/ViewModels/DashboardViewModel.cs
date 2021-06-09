fb9a6d45a15180ab6d2215414d09b34d2b2ff714
Israel Calderón de la Cruz <icalderond@outlook.com>
2021-06-08 21:10:52 -0500

    Connecting viewmodel with view

---
+++ ServiceRequest/ServiceRequest/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,61 @@
+﻿using ServiceRequest.Models;
+using ServiceRequest.Services;
+using System;
+using System.Collections.ObjectModel;
+
+namespace ServiceRequest.ViewModels
+{
+    public class DashboardViewModel : BindableBase
+    {
+        #region [ Constructor ]
+        public DashboardViewModel()
+        {
+            LoadData();
+        }
+
+        #region [ Methods ]
+        private async void LoadData()
+        {
+            try
+            {
+                IsBusy = true;
+                var listRequests = await SrListService.GetSrList();
+                RequesModelList = new ObservableCollection<RequestModel>(listRequests);
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", ex.Message, "Enterado");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+        #endregion [ Methods ]
+
+        #endregion [ Constructor ]
+
+        #region [ Properties ]
+        private ObservableCollection<RequestModel> _RequesModelList;
+        public ObservableCollection<RequestModel> RequesModelList
+        {
+            get => _RequesModelList;
+            set => Set(ref _RequesModelList, value);
+        }
+
+        private SrListService _SrListService;
+
+        public SrListService SrListService
+        {
+            get { return _SrListService = _SrListService ?? new SrListService(); }
+        }
+
+        private bool _IsBusy;
+        public bool IsBusy
+        {
+            get => _IsBusy;
+            set => Set(ref _IsBusy, value);
+        }
+        #endregion [ Properties ]
+    }
+}
--- ServiceRequest/ServiceRequest/Views/DashboardPage.xaml.cs
+++ ServiceRequest/ServiceRequest/Views/DashboardPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using ServiceRequest.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +10,7 @@ namespace ServiceRequest.Views
         public DashboardPage()
         {
             InitializeComponent();
+            this.BindingContext = new DashboardViewModel();
         }
     }
 }
